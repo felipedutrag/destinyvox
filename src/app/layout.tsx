adbecb9798c9cc4cdecaf3f9026c2ec889c2d588
felipedutrag <felipedutra@outlook.com>
2026-09-11 01:47:02 -0300

    feat(landing): create custom hermetic gold star favicon and apple-touch-icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="apple-touch-icon" href="/favicon.svg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
